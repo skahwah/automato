4ac82e6fe5f4b02e6a4dc721bb09e7eee4662043
Sanjiv Kawa <sanjivkawa@gmail.com>
2019-05-29 20:53:30 -0400

    Update automato.rb
    added support for secure ldap v2.1 stamp

--- automato.rb
+++ automato.rb
@@ -8,7 +8,7 @@ require './helpers/cli.rb'
 require './helpers/ldap_querier.rb'
 require './helpers/smb_querier.rb'
 
-puts "automato v2.0.1"
+puts "automato v2.1"
 puts "Written by: Sanjiv Kawa"
 puts "Twitter: @kawabungah"
 puts ""
